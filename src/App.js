ed5c66dc7dd486fb4d131087c54c88ef68dea586
Sahib <sahibjohar@gmail.com>
2023-01-07 04:03:34 -0500

    Almost Finished Build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -29,11 +32,29 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder Function
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? 
+    {...task, reminder: !task.reminder} : task))
+  }
+
+  // Add Task
+  const addTask = (task) => {
+    const id = tasks[tasks.length - 1].id + 1;
+    const newTask = {id, ...task};
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => {
+          setShowAddTask(!showAddTask);
+        }}
+        showAdd={showAddTask}
+      />
+      {showAddTask ? <AddTask onAdd={addTask} /> : ''}
       {tasks.length > 0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask}/> : 'You\'ve completed all your tasks.'}
+      onDelete={deleteTask} onToggle={toggleReminder} /> : 'You\'ve completed all your tasks.'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,15 @@
 import Button from './Button'
-import App from '../App'
 
 const Header = (props) => {
-    const onClick = () => {
-        console.log('hi');
-        App.tasks.setTasks({
-            id: 4,
-            text: 'Take Dog Out',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        });
-      }
       
   return (
     <header className = 'header'>
         <h1>{props.title}</h1>
-        <Button onClick={onClick} color='green' text='Add'/>
+        <Button 
+            onClick={props.onAdd} 
+            color='black' 
+            text={!props.showAdd ? 'Add Task' : 'Close'}
+        />
     </header>
   )
 }
